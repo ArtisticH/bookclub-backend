dcf09702de6d730766ca24f1fc3cbc7bd8651b9c
서한나 <hanna@seohannaui-MacBookAir.local>
2024-08-06 16:20:25 +0900

    t

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cooikeParser(process.env.COOKIE_SECRET));
